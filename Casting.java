3f22fc2d8db094df80d37e57644a434ed38890c0
Balaji11022001 <85121690+Balaji11022001@users.noreply.github.com>
2023-12-28 18:01:20 +0530

    Add files via upload

---
+++ Casting.java
@@ -0,0 +1,22 @@
+package Javaproject;
+
+public class Casting {
+
+	public static void main(String[] args) {
+		// Narrow typecasting
+		double num = 10.99;
+	    System.out.println("The double value: " + num);
+
+	    // convert into int type
+	    int data = (int)num;
+	    System.out.println("The integer value: " + data);
+	 // create string type variable
+	    String da = "10";
+	    System.out.println("The string value is: " + data);
+
+	    // convert string variable to int
+	    int nu = Integer.parseInt(da);
+	    System.out.println("The integer value is: " + nu);
+	}
+
+}
